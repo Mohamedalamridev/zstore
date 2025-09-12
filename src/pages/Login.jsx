61a4571076866d21a3b635c0ff5f4fea45c59957
muhammedAlamri <mohamedalamri.dev@gmail.com>
2025-09-12 11:45:50 +0300

    add login, signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
   return (
     <>
       <Navbar />
-      <main className="w-full min-h-screen flex items-center justify-center bg-gray-100">
+      <main className="w-full p-4 min-h-screen flex items-center justify-center bg-gray-100">
         <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md">
           <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">
             Welcome Back
