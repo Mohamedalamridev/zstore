e60fb4829010b2083fe1c39520e1b514f1cac7d5
muhammedAlamri <mohamedalamri.dev@gmail.com>
2025-09-13 19:35:43 +0300

    change base url for payment

--- src/pages/PaymentCheckout.jsx
+++ src/pages/PaymentCheckout.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 function PaymobCheckout({ cartItems, totalAmount, userId }) {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
   const handleCheckout = async () => {
     const items = cartItems.map((item) => {
       return {
         name: item.title,
-        amount_cents: String(item.price * 100), // ✅ بالقروش
+        amount_cents: String(item.price * 100),
         description: item.info.join(", "),
         quantity: String(item.count),
       };
     });
     try {
-      const res = await fetch("http://localhost:3000/api/payment/checkout", {
+      const res = await fetch(`${baseUrl}/api/payment/checkout`, {
         method: "POST",
         headers: { "Content-Type": "Application/json" },
         credentials: "include",
