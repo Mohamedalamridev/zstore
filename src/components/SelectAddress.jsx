708b465834e9a8ac50a67dabc20c85362d256b59
muhammedAlamri <mohamedalamri.dev@gmail.com>
2025-09-18 16:50:51 +0300

    add address to cart

--- src/components/SelectAddress.jsx
+++ src/components/SelectAddress.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-export const Feild = ({ street, placeholder, onchange }) => {
+export const Feild = ({ name, placeholder, value, onchange }) => {
   return (
     <input
       type="text"
-      name={street}
+      name={name}
       onChange={onchange}
+      value={value}
       placeholder={placeholder}
       className="border-[2px] border-gray-300 p-3 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none w-full mb-4"
     />
@@ -22,6 +23,31 @@ function SelectAddress() {
     street: "",
     phone: "",
   });
+
+  const handleAddAddress = async (e) => {
+    e.preventDefault();
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+
+    try {
+      const res = await fetch(`${baseUrl}/api/user/profile/address`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(address),
+        credentials: "include",
+      });
+      const result = await res.json();
+      console.log(result);
+
+      if (res.ok) {
+        alert("Address added successfully");
+      } else {
+        alert(result.message || "Failed to add address");
+      }
+    } catch (err) {
+      console.error("Add address failed:", err);
+    }
+  };
+
   return (
     <div className="mb-6">
       <h1 className="font-bold text-xl mb-3">Add Address</h1>
@@ -59,7 +85,7 @@ function SelectAddress() {
           }
         />
         <Feild
-          name="zip"
+          name="postalCode"
           placeholder="ZIP/Postal Code"
           value={address.postalCode}
           onchange={(e) =>
@@ -84,6 +110,7 @@ function SelectAddress() {
         />
 
         <button
+          onClick={handleAddAddress}
           type="submit"
           className=" mt-2 px-4 py-2 rounded-xl bg-black text-white font-semibold hover:bg-blue-700 transition"
         >
